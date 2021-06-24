d5bab85bcb3ca02a204490a3887b1239cb62d875
Nataniel <nataniel-.louis@epitech.eu>
2021-06-25 00:55:24 +0200

    now working as expected, lists everything if no flag

--- skip-synthesis.sh
+++ skip-synthesis.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/bash
 
-if [ -z $1 ]
-then
-    exit 84
-fi
-
-flag=${1}
-flag_len=${#flag}
-
 read a
 while read b c d e f g h i j
 do
-    if [ $flag == $(echo "$j" | cut -b -${flag_len} -) ]
+    if [ -z $1 ]
     then
         printf "%s\t%s\n" "$d" "$j"
+    else
+        flag=${1}
+        flag_len=${#flag}
+        if [ $flag == $(echo "$j" | cut -b -${flag_len} -) ]
+        then
+            printf "%s\t%s\n" "$d" "$j"
+        fi
     fi
 done
